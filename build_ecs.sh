8b0973a21c13bd6c957d43c9b546612fe575d783
Nick Williams <enjeyw@gmail.com>
2017-11-13 18:34:26 +1100

    add ecs build script

---
+++ build_ecs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+REPOSITORY_URI=290492953667.dkr.ecr.ap-southeast-2.amazonaws.com/databoilerplate
+
+npm run build
+
+docker build -t server .
+docker build -t proxy ./proxy
+
+eval $(aws ecr get-login --no-include-email --region ap-southeast-2 --profile ECS);
+
+docker tag proxy:latest $REPOSITORY_URI:proxy
+docker push $REPOSITORY_URI:proxy
+
+docker tag server:latest $REPOSITORY_URI:server
+docker push $REPOSITORY_URI:server
