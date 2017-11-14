95d5ea743a78f5f73a21eed389d471a8ae062324
Nick Williams <enjeyw@gmail.com>
2017-11-14 19:07:35 +1100

    Added migration

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ location = os.environ.get('LOCATION')
 
 if location == "PROD":
     parser.read(os.path.join(basedir,'config_files/prod_config.ini'))
-elif os.environ['LOCATION'] == "LOCAL_DOCKER":
+elif location == "LOCAL_DOCKER":
     parser.read(os.path.join(basedir,'config_files/local_docker_config.ini'))
 else:
     parser.read(os.path.join(basedir,'config_files/local_config.ini'))
